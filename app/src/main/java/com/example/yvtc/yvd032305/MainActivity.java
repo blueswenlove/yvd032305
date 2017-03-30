ad2773356a3b0a8814e853d1e1de0fa9970c0f94
blueswen <blueswenlove@gamil.com>
2017-03-30 11:22:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd032305/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.yvtc.yvd032305;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textView);
+        registerForContextMenu(tv);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if(tv.getId()==R.id.textView){
+            menu.add("AA");
+            menu.add("BB");
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.menuadd:
+
+                break;
+            case R.id.menusettings:
+
+                break;
+            case R.id.menuabout:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
